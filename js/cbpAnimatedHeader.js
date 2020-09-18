752a97900cbfb9d0eab576eb6d928a9768c0dd09
azqa <11bscsanadeem@seecs.edu.pk>
2020-09-18 19:33:26 +0200

    testing pictures

--- js/cbpAnimatedHeader.js
+++ js/cbpAnimatedHeader.js
@@ -26,12 +26,12 @@ var cbpAnimatedHeader = (function() {
 
 	function scrollPage() {
 		var sy = scrollY();
-		if ( sy >= changeHeaderOn ) {
-			classie.add( header, 'navbar-shrink' );
-		}
-		else {
-			classie.remove( header, 'navbar-shrink' );
-		}
+		//if ( sy >= changeHeaderOn ) {
+		//	classie.add( header, 'navbar-shrink' );
+		//}
+		//else {
+		//	classie.remove( header, 'navbar-shrink' );
+		//}
 		didScroll = false;
 	}
 
